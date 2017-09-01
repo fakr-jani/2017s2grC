812c9c6d94e74257b14c04980422964f3edb4ac7
orsolyanemet <31074955+orsolyanemet@users.noreply.github.com>
2017-09-01 09:25:00 +0300

    Update UserDAOImpl.java

--- jbugs-persistence/src/main/java/operations/UserDAOImpl.java
+++ jbugs-persistence/src/main/java/operations/UserDAOImpl.java
@@ -6,12 +6,14 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Transient;
 
 import entities.User;
 
 @Stateless(name = "UserDAOImpl")
 public class UserDAOImpl implements UserDAO, Serializable {
 
+	@Transient
 	@PersistenceContext(unitName = "jbugs-persistence")
 	EntityManager em;
 
