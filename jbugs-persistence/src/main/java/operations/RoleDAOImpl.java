84821582bf6905dbf8eae131e8b9767530da0943
orsolyanemet <31074955+orsolyanemet@users.noreply.github.com>
2017-09-01 09:02:20 +0300

    Update RoleDAOImpl.java

--- jbugs-persistence/src/main/java/operations/RoleDAOImpl.java
+++ jbugs-persistence/src/main/java/operations/RoleDAOImpl.java
@@ -13,6 +13,7 @@ import entities.Role;
 @Stateless(name = "RoleDAOImpl")
 public class RoleDAOImpl implements RoleDAO, Serializable {
 
+	@Serializable
 	@PersistenceContext(unitName = "jbugs-persistence")
 	EntityManager em;
 
