0cec513ab61b433a32e18ed54aa04474ce561bd7
Patricia Ardelean <ardelean.patricia@gmail.com>
2017-09-04 11:00:48 +0300

    Initial version.

---
+++ jbugs-business/src/main/java/edu/msg/ro/business/dto/UserDTO.java
@@ -0,0 +1,85 @@
+package edu.msg.ro.business.dto;
+
+public class UserDTO {
+
+	private Long id;
+
+	private String firstname;
+
+	private String lastname;
+
+	private boolean active;
+
+	private String username;
+
+	private String password;
+
+	private String email;
+
+	private String phoneNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
