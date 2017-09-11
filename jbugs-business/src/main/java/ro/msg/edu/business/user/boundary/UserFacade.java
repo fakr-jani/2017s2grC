dc29f24859ee07e322fcd86e6acdd7d1579fcf4e
Bianca Mares <bianca.mares13@yahoo.com>
2017-09-11 18:20:47 +0300

    UserFacade conflicts solved

--- jbugs-business/src/main/java/ro/msg/edu/business/user/boundary/UserFacade.java
+++ jbugs-business/src/main/java/ro/msg/edu/business/user/boundary/UserFacade.java
@@ -1,15 +1,11 @@
 package ro.msg.edu.business.user.boundary;
 
-<<<<<<< HEAD
 import java.util.List;
 
-=======
->>>>>>> cb0ce5acfce2edaa4ea7666a1d5578bcd568be67
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-<<<<<<< HEAD
 import javax.inject.Inject;
 
 import ro.msg.edu.business.common.exception.TechnicalException;
@@ -17,12 +13,6 @@ import ro.msg.edu.business.user.control.UserCRUDControl;
 import ro.msg.edu.business.user.dao.UserDAO;
 import ro.msg.edu.business.user.dto.UserDTO;
 import ro.msg.edu.business.user.dto.mapper.UserDTOMapper;
-=======
-
-import ro.msg.edu.business.common.exception.BusinessException;
-import ro.msg.edu.business.user.control.UserCRUDControl;
-import ro.msg.edu.business.user.dto.UserDTO;
->>>>>>> cb0ce5acfce2edaa4ea7666a1d5578bcd568be67
 
 /**
  * Boundary for user component.
@@ -37,7 +27,6 @@ public class UserFacade {
 	@EJB
 	private UserCRUDControl userCRUDControl;
 
-<<<<<<< HEAD
 	@Inject
 	private UserDTOMapper userDTOMapper;
 
@@ -45,9 +34,6 @@ public class UserFacade {
 	private UserDAO userDAO;
 
 	public UserDTO createUser(UserDTO user) throws TechnicalException {
-=======
-	public UserDTO createUser(UserDTO user) throws BusinessException {
->>>>>>> cb0ce5acfce2edaa4ea7666a1d5578bcd568be67
 		return userCRUDControl.createUser(user);
 	}
 
@@ -56,7 +42,6 @@ public class UserFacade {
 
 	}
 
-<<<<<<< HEAD
 	public UserDTO activateUser(UserDTO userDTO) {
 		return userCRUDControl.activateUser(userDTO);
 
@@ -81,10 +66,4 @@ public class UserFacade {
 		return userCRUDControl.findAllUser();
 	}
 
-=======
-	public UserDTO updateUser(UserDTO userDTO) throws BusinessException {
-		return userCRUDControl.updateUser(userDTO);
-	}
-
->>>>>>> cb0ce5acfce2edaa4ea7666a1d5578bcd568be67
 }
